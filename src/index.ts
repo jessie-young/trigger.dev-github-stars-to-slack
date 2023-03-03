58e53084812231ec8dd8f4bc83c12a676795ec80
Jessie Young <jessie@sahale.io>
2023-03-03 00:34:03 -0800

    dummy commit

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ new Trigger({
 
     await slack.postMessage("⭐️", {
       channelName: "test",
-      text: `Woohoo!! I got a new GitHub star from \n<${event.sender.html_url}|${event.sender.login}>. You now have ${event.repository.stargazers_count} stars!`,
+      text: `Yay!!! :)  I got a new GitHub star from \n<${event.sender.html_url}|${event.sender.login}>. You now have ${event.repository.stargazers_count} stars!`,
     });
   },
 }).listen();
