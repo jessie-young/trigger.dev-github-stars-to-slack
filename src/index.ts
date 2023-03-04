bd9b7af31b86709159579c96f74d1483fcbfc824
Jessie Young <jessie@sahale.io>
2023-03-03 20:32:18 -0800

    pushed test of file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,47 @@
 import { Trigger } from "@trigger.dev/sdk";
 import * as github from "@trigger.dev/github";
 import * as slack from "@trigger.dev/slack";
+import fs from 'fs';
 
 //const repo = process.env.GITHUB_REPOSITORY ?? "triggerdotdev/github-stars-to-slack";
 const repo = process.env.GITHUB_REPOSITORY
+
+const folderName = '/data';
+
+try {
+  if (!fs.existsSync(folderName)) {
+    console.log("folder " + folderName + " doesn't exist; creating")
+    fs.mkdirSync(folderName);
+    fs.mkdirSync(folderName + "/sub");
+    fs.writeFile('/data/sub/hello.txt', 'I am cool!',  function(err) {
+      if (err) {
+          return console.error(err);
+      }
+      console.log("File created!");
+    });
+  } else {
+    console.log("folder exists; creating /sub folder")
+    fs.mkdirSync(folderName + "/sub");
+    fs.writeFile('/data/sub/hello.txt', 'I am cool!',  function(err) {
+      if (err) {
+          return console.error(err);
+      }
+      console.log("File created!");
+    });
+
+  }
+} catch (err) {
+  console.error(err);
+}
+
+
+fs.readFile('/data/sub/hello.txt', function (err, data) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("Asynchronous read: " + data.toString());
+});
+
 
 new Trigger({
   id: "github-stars-to-slack",
@@ -22,3 +60,4 @@ new Trigger({
     });
   },
 }).listen();
+
