93361214849b38f40e9f930a3d44a18f691fd7aa
Jessie Young <jessie@sahale.io>
2023-03-15 10:28:32 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Trigger } from "@trigger.dev/sdk";
+import * as github from "@trigger.dev/github";
+import * as slack from "@trigger.dev/slack";
+
+// Change "triggerdotdev/github-stars-to-slack" to the repo you want to track e.g. "yourorg/yourrepo"
+const repo =
+  process.env.GITHUB_REPOSITORY ?? "triggerdotdev/github-stars-to-slack";
+
+new Trigger({
+  id: "github-stars-to-slack",
+  name: "GitHub Stars to Slack",
+  on: github.events.newStarEvent({
+    repo,
+  }),
+  run: async (event) => {
+    await slack.postMessage("⭐️", {
+      channelName: "github-stars",
+      text: `New GitHub star from \n<${event.sender.html_url}|${event.sender.login}>. You now have ${event.repository.stargazers_count} stars!`,
+    });
+  },
+}).listen();
