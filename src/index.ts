0a9667fa0a74009350df379567b55561c1ca7c64
Jessie Young <84425236+jessie-young@users.noreply.github.com>
2023-03-21 09:46:22 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ new Trigger({
   run: async (event) => {
     await slack.postMessage("⭐️", {
       channelName: "test",
-      text: `yay! New GitHub star from \n<${event.sender.html_url}|${event.sender.login}>. You now have ${event.repository.stargazers_count} stars! YAY!`,
+      text: `New GitHub star from \n<${event.sender.html_url}|${event.sender.login}>. You now have ${event.repository.stargazers_count} stars! YAY!`,
     });
   },
 }).listen();
